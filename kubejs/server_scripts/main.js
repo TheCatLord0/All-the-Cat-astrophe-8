ee1993cd9819a4f9229b33df6cf3cdfec356327f
TheCatLord0 <lordnummies@gmail.com>
2026-05-09 17:46:51 -0500

    Added main.js and ban list

---
+++ kubejs/server_scripts/main.js
@@ -0,0 +1,44 @@
+// Banned items
+let bannedItems = [
+   'easy_villagers:auto_trader',
+   'easy_villagers:iron_farm' 
+]
+
+PlayerEvents.inventoryChanged(event => {
+  let player = event.player;
+  let inventory = player.inventory.items; // NeoForge inventory access
+
+  if (!bannedItems.includes(event.item.id)) return;
+
+  // Alert the player
+  player.tell([
+    Text.darkRed("[Alert] ").bold(), 
+    Text.gold(event.item.id).bold(), 
+    Text.gray(" has been deleted."),
+    "\n",
+    Text.green("If you have questions why, ask thecatlord0 on Discord.")
+  ])
+
+  // Remove all instances of the banned item
+  for (let i = 0; i < inventory.length; i++) {
+    let slotItem = inventory[i];
+    if (slotItem && slotItem.id === event.item.id) {
+      slotItem.count = 0 // Clears the stack
+    }
+  }
+})
+LootJS.modifiers(event => {
+  bannedItems.forEach(bannedItems => {
+    event.addTableModifier(/.*/).removeLoot(bannedItems)
+  })
+})
+ServerEvents.recipes(event => {
+  bannedItems.forEach(bannedItems => {
+    event.remove({output: bannedItems })
+  })
+})
+ServerEvents.tags('item', event => {
+  bannedItems.forEach(bannedItems => {
+    event.add('cat:removal', bannedItems)
+  })
+})
