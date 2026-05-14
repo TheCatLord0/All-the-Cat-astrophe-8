7ab65a6a9cc2844b9e14f6ce5a5e78049af93919
TheCatLord0 <lordnummies@gmail.com>
2026-05-14 01:50:54 -0500

    Fixed the archfruit balancing

--- kubejs/startup_scripts/main.js
+++ kubejs/startup_scripts/main.js
@@ -1,15 +1,144 @@
+import { effect } from "@package/net/minecraft/world"
+
+const $MobEffectInstance = Java.loadClass('net.minecraft.world.effect.MobEffectInstance')
+
 ItemEvents.modification(event => {
-let nerfedArchFruit = [
-    "ars_nouveau:mendosteen_pod",
-    "ars_nouveau:bastion_pod",
-    "ars_nouveau:bombegranate_pod",
-    "ars_nouveau:frostaya_pod",
-    "ars_elemental:flashpine_pod",
-]
-  event.modify(nerfedArchFruit, item => {
-    item.foodProperties = food => {
-        food.hunger(2)
-        food.saturation(0.4)
-    }
+  event.modify("ars_nouveau:mendosteen_pod", item => {
+    item.setFood({
+        nutrition: 2,
+        saturation: 0.4,
+        eatSeconds: 1.6,
+        canAlwaysEat: true,
+        effects: [
+        {
+          probability: 1,
+          effectSupplier: () =>
+            new $MobEffectInstance(
+              /* Effect:         */ "ars_nouveau:recovery",
+              /* Duration:       */ 60 * 20,
+              /* Level:          */ 0,
+              /* Is ambient:     */ false,
+              /* Hide particles: */ true
+            )
+        }
+      ]
+    })
+  })
+  event.modify("ars_nouveau:frostaya_pod", item => {
+    item.setFood({
+        nutrition: 2,
+        saturation: 0.4,
+        eatSeconds: 1.6,
+        canAlwaysEat: true,
+        effects: [
+        {
+          probability: 1,
+          effectSupplier: () =>
+            new $MobEffectInstance(
+              /* Effect:         */ "ars_nouveau:freezing",
+              /* Duration:       */ 30 * 20,
+              /* Level:          */ 0,
+              /* Is ambient:     */ false,
+              /* Hide particles: */ true
+            )
+        }
+      ]
+    })
+  })
+  event.modify("ars_nouveau:bastion_pod", item => {
+    item.setFood({
+        nutrition: 2,
+        saturation: 0.4,
+        eatSeconds: 1.6,
+        canAlwaysEat: true,
+        effects: [
+        {
+          probability: 1,
+          effectSupplier: () =>
+            new $MobEffectInstance(
+              /* Effect:         */ "ars_nouveau:shielding",
+              /* Duration:       */ 60 * 20,
+              /* Level:          */ 0,
+              /* Is ambient:     */ false,
+              /* Hide particles: */ true
+            )
+        }
+      ]
+    })
+  })
+  event.modify("ars_nouveau:bombegranate_pod", item => {
+    item.setFood({
+        nutrition: 2,
+        saturation: 0.4,
+        eatSeconds: 1.6,
+        canAlwaysEat: true,
+        effects: [
+        {
+          probability: 1,
+          effectSupplier: () =>
+            new $MobEffectInstance(
+              /* Effect:         */ "ars_nouveau:blasting",
+              /* Duration:       */ 10 * 20,
+              /* Level:          */ 0,
+              /* Is ambient:     */ false,
+              /* Hide particles: */ true
+            )
+        }
+      ]
+    })
+  })
+  event.modify("ars_elemental:flashpine_pod", item => {
+    item.setFood({
+        nutrition: 2,
+        saturation: 0.4,
+        eatSeconds: 1.6,
+        canAlwaysEat: true,
+        effects: [
+        {
+          probability: 1,
+          effectSupplier: () =>
+            new $MobEffectInstance(
+              /* Effect:         */ "minecraft:night_vision",
+              /* Duration:       */ 30 * 20,
+              /* Level:          */ 0,
+              /* Is ambient:     */ false,
+              /* Hide particles: */ true
+            )
+        },
+        {
+          probability: 1,
+          effectSupplier: () =>
+            new $MobEffectInstance(
+              /* Effect:         */ "minecraft:glowing",
+              /* Duration:       */ 30 * 20,
+              /* Level:          */ 0,
+              /* Is ambient:     */ false,
+              /* Hide particles: */ true
+            )
+        },
+        {
+          probability: 1,
+          effectSupplier: () =>
+            new $MobEffectInstance(
+              /* Effect:         */ "ars_nouveau:shocked",
+              /* Duration:       */ 30 * 20,
+              /* Level:          */ 0,
+              /* Is ambient:     */ false,
+              /* Hide particles: */ true
+            )
+        },
+        {
+          probability: 1,
+          effectSupplier: () =>
+            new $MobEffectInstance(
+              /* Effect:         */ "ars_elemental:static_charged",
+              /* Duration:       */ 30 * 20,
+              /* Level:          */ 0,
+              /* Is ambient:     */ false,
+              /* Hide particles: */ true
+            )
+        }
+      ]
+    })
   })
 })
